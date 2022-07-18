591db57d2824c6de27f4c4a05f61826ffa503bf8
Lucas Mendes <devsdmf@gmail.com>
2022-07-18 18:51:12 -0300

    First commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import * as FlexPlugin from '@twilio/flex-plugin';
+
+import FlexInactiveChatChannelPlugin from './FlexInactiveChatChannelPlugin';
+
+FlexPlugin.loadPlugin(FlexInactiveChatChannelPlugin);
